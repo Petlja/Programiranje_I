ac130d5fa936ff1e96b42a99064bd13e9a8bb66c
Lazar Bojičić <34066720+lazarbojicic@users.noreply.github.com>
2020-08-24 16:51:40 +0200

    added pygame

---
+++ _includes/mesec_i_zvezdice.py
@@ -0,0 +1,29 @@
+# -*- acsection: general-init -*-
+import random
+import pygame as pg
+import pygamebg
+
+(sirina, visina) = (300, 300) # otvaramo prozor
+prozor = pygamebg.open_window(sirina, visina, "Ноћно небо")
+
+# -*- acsection: main -*-
+
+# bojimo pozadinu prozora u crno
+prozor.fill(pg.Color("black"))
+
+# crtamo 100 nasumično raspoređenih zvezdica
+for i in range(100):
+    # nasumično određujemo koordinate
+    x = random.randint(0, sirina)
+    y = random.randint(0, visina)
+    # crtamo zvezdicu kao mali kružić
+    pg.draw.circle(prozor, pg.Color("white"), (x, y), 2)
+
+# crtamo mlad mesec pomoću jednog crnog i jednog belog kruga
+pg.draw.circle(prozor, pg.Color("white"), (100, 100),  30)
+pg.draw.circle(prozor, pg.Color("black"), (120, 100),  30)
+
+# -*- acsection: after-main -*-
+
+# prikazujemo prozor i čekamo da ga korisnik isključi
+pygamebg.wait_loop()
